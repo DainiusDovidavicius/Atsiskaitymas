026c09ab26fbb9061a94d9ba230d8d745ca4a6d8
DainiusDovidavicius <dainiusdovidavicius@gmail.com>
2023-03-10 19:33:28 +0200

    Fetching posts from json placeholder

--- src/pages/Newspage.jsx
+++ src/pages/Newspage.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 
 function Newspage() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <div>
       <Header />
 
-      <h1>Welcome to news page</h1>
+      <div>
+        <h1>Posts</h1>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2> <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
